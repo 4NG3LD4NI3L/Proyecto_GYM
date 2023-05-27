244a6d63330c082d22bdaee5048ddf4915931ad4
4NG3LD4NI3L <aromero_21@alu.uabcs.mx>
2023-05-27 10:44:00 -0700

    Clase ventana y para importar imagenes

---
+++ src/Imagenes.java
@@ -0,0 +1,26 @@
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Imagenes {
+	private String ruta = "Resources/";
+	
+    public Imagenes(String nombre, JPanel pantalla) {
+        ImageIcon imagen = new ImageIcon(ruta + nombre);
+        JLabel carga = new JLabel(imagen);
+        pantalla.add(carga);
+    }
+
+    public Imagenes(String nombre, int ancho, int alto, JPanel pantalla) {
+        ImageIcon imagen = new ImageIcon(ruta + nombre);
+        Image imagenGif = imagen.getImage();
+
+        // Redimensiona la imagen
+        Image imagenRedimensionada = imagenGif.getScaledInstance(ancho, alto, Image.SCALE_DEFAULT);
+        ImageIcon imagen2 = new ImageIcon(imagenRedimensionada);
+        JLabel carga = new JLabel(imagen2);
+        pantalla.add(carga);
+       
+    }  
+}
---
+++ src/Ventana.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame {
+    
+    public Ventana(){
+        this.setVisible(true);
+        this.setLayout(null);
+        this.setSize(800,600);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+    }
+}
